ef7c72511cef9f1f34150adeb46429e7a112086b
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-04-14 10:53:11 +0200

    Breaking: Use sonarqube 8.2 community image

--- devcontrol/global/startup.sh
+++ devcontrol/global/startup.sh
@@ -4,5 +4,5 @@ set -eu
 
 # @file devcontrol/global/startup.sh
 # @brief devcontrol startup script and functions
-echo "Generic Platform - SonarQube Service (c) Teecke 2020"
+echo "Generic Platform - SonarQube Service (c) 2020 TIC para Bien"
 echo
